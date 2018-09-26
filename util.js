9de05a9cf33bdcaca469d51e59e4ef07488d37fd
renjie <renjie@zijjj.com>
2018-09-26 15:52:38 +0800

    添加 secToTime和timeToSec函数

--- util.js
+++ util.js
@@ -274,46 +274,45 @@ function deepMerge (obj1, obj2) {
 }
 
 /**
- * 时间秒数格式化
- * @param s 时间戳（单位：秒）
- * @returns {*} 格式化后的时分秒
+ * 秒数转时间字符串
+ * @param {[Number]} [s] ['秒数 如：86400']
+ * @returns {String} 格式化后的时分秒，如：'24:00:00.00'
  */
-var sec_to_time = function(s) {
-    var t;
+function secToTime (s) {
+    var t
     if(s > -1){
-        var hour = Math.floor(s/3600);
-        var min = Math.floor(s/60) % 60;
-        var sec = s % 60;
+        var hour = Math.floor(s / 3600)
+        var min = Math.floor(s / 60) % 60
+        var sec = s % 60
         if(hour < 10) {
-            t = '0'+ hour + ":";
+            t = '0'+ hour + ":"
         } else {
-            t = hour + ":";
+            t = hour + ":"
         }
 
-        if(min < 10){t += "0";}
-        t += min + ":";
-        if(sec < 10){t += "0";}
-        t += sec.toFixed(2);
+        if(min < 10){t += "0"}
+        t += min + ":"
+        if(sec < 10){t += "0"}
     }
-    return t;
+    return t
 }
 
 /**
- * 时间转为秒
- * @param time 时间(00:00:00)
- * @returns {string} 时间戳（单位：秒）
+ * 时间字符串转为秒
+ * @param {String} time 时间字符串 '24:00:00'
+ * @returns {Number} 秒数 如：86400
  */
-var time_to_sec = function (time) {
-    var s = '';
+function timeToSec (time) {
+    var s = ''
 
-    var hour = time.split(':')[0];
-    var min = time.split(':')[1];
-    var sec = time.split(':')[2];
+    var hour = time.split(':')[0]
+    var min = time.split(':')[1]
+    var sec = time.split(':')[2]
 
-    s = Number(hour*3600) + Number(min*60) + Number(sec);
+    s = Number(hour * 3600) + Number(min * 60) + Number(sec)
 
-    return s;
-};
+    return s
+}
 
 
 //导出
@@ -333,6 +332,6 @@ export default {
     is_forbid,
     queryStringToObject,
     objectToQueryString,
-    sec_to_time,
-    time_to_sec
+    secToTime,
+    timeToSec
 }
